6933a45b224e36166a8801979175fa497dbaabd6
陈美辛 <cmx02088102@alibaba-inc.com>
2025-05-08 16:34:00 +0800

    feat: add setup.py for package management

---
+++ setup.py
@@ -0,0 +1,19 @@
+# setup.py
+from setuptools import setup, find_packages
+
+setup(
+    name="langchain-GLM_agent",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=[
+        'pandas',
+    ],
+    extras_require={
+        'dev': [
+            'sphinx',
+            'sphinx-rtd-theme',
+            'myst-parser',
+            # 其他文档相关的依赖
+        ]
+    }
+)
